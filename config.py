6e408e17f39f3cf2fa28527437db0a32c456ae28
Gaurav Chaudhari <capristar02@gmail.com>
2020-05-31 17:49:59 +0530

    Remove secret key

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-GOOGLE_CLIENT_ID = "1049143987323-qli96rad5grt13328robulgajan69vg9.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "WuMqmsSLcde0q1yVjoDJ4T7b"
+GOOGLE_CLIENT_ID = <>
+GOOGLE_CLIENT_SECRET = <>
